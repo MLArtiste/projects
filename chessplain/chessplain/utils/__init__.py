3ef558122eda727a1e341f5091cd399f60c692ba
MLA <mlartiste@proton.me>
2026-08-11 21:59:52 +0100

    fix: use build_annotated_fens instead of build_commentary_data in __init__.py

--- chessplain/chessplain/utils/__init__.py
+++ chessplain/chessplain/utils/__init__.py
@@ -1,14 +1,14 @@
-from .data_extractor import build_train_data, build_val_data, build_commentary_data
+from .data_extractor import build_train_data, build_val_data, build_annotated_fens
 from .uci_vocab import IDX_TO_UCI, UCI_TO_IDX
 from .tokenizer import FENTokenizer
 from .engine import ChessNetEngine
 
 __all__ = [
-    "build_commentary_data", 
-    "build_train_data", 
-    "build_val_data", 
-    "IDX_TO_UCI", 
+    "build_annotated_fens",
+    "build_train_data",
+    "build_val_data",
+    "IDX_TO_UCI",
     "UCI_TO_IDX",
-    "ChessNetEngine", 
+    "ChessNetEngine",
     "FENTokenizer",
 ]
